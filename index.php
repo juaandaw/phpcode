94c779f03a53707c9d568a6034d68c9258c588dd
juaandaw <72797182+juaandaw@users.noreply.github.com>
2020-10-13 12:40:16 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php ini_set('display_errors',1);?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width, initial-scale=1.0">
+	<title>Ejercicios</title>
+</head>
+<body>
+	<h1>Resolucion de Ejercicios</h1>
+	<br>
+	<ul>
+		<li>
+			<h3>Hoja de ejercicios nº1</h3>
+			<ul>
+                <li><a href="/hoja1/Ejercicio10.php">Ejercicio 10</a></li>
+            </ul>
+        </li>
+    </ul>
+    <li>
+        <h3>Hoja de ejercicios nº2</h3>
+        <ul>
+            <li><a href="/hoja2/Ejercicio4_Hoja2.php">Ejercicio 4</a></li>
+        </ul>
+    </li>
+
+    <li>Ejemplos del tema 4</li>
+        <h3>Ejemplos del tema 4</h3>
+        <ul>
+            <li><a href="/tema4/get.php">Ejemplos de uso de $_GET</a></li></li>
+            <li><a href="/tema4/formulario.php">Ejemplos de uso de $_POST</a></li></li>
+            <li><a href="/tema4/post.php">Ejemplos de uso de $_POST en un unico fichero</a></li></li>
+        </ul>
+    <li>
+            <h3>Ejemplos del tema 5</h3>
+            <ul>
+                <li><a href="/tema5/cookies.php">Cookies</a></li>
+                <li><a href="/tema5/sesion1.php">Sesiones</a></li>
+                <li><a href="/tema5/sesion2.php">Sesiones2</a></li>
+
+            </ul>
+        </li>
+    <li>
+        <h3>Ejercicio Propuesto</h3>
+        <ul>
+            <li><a href="/EjercicioPropuesto/Autenticacion.php">Autenticacion</a></li>
+        </ul>
+    </li>
+     <li>
+        <h3>Ejemplos tema6</h3>
+        <ul>
+            <li><a href="/tema6/index.php">Version1</a></li>
+            <li><a href="/tema6/index2.php">Version2</a></li>
+            <li><a href="/tema6/index3.php">Version3</a></li>
+            <li><a href="/tema6/index4.php">Version4</a></li>
+            <li><a href="/tema6/index5.php">Version5</a></li>
+            <li><a href="/tema6/index6.php">6</a></li>
+        </ul>
+         <h3>Ejemplos tema9</h3>
+    </li>
+<ul>
+    <li><a href="/tema9/index.php">Ejemplo1</a></li>
+    <li><a href="/tema9/citas.php">Citas Medicas</a></li>
+    <li><a href="/tema9/upload.php">Upload</a></li>
+</ul>
+        <h3>Ejemplos tema11</h3>
+<ul>
+    <li><a href="/tema11/Freelance.php">Freelance</a></li>
+</ul>
+<ul>
+    <h3>Ejercicio Propuesto</h3>
+    <li><a href="/tema11/Enlace.php">Enlace</a></li>
+</ul>
+
+
+
+
+
+
+</body>
+</html>
+
